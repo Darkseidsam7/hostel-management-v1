a0cfb6f0317c7e50dfa197f2df2e6412e121d359
Sushant Mohite <69734778+Darkseidsam7@users.noreply.github.com>
2021-04-05 02:39:10 +0530

    Updated Guideline - Contact

--- guidelines.sql
+++ guidelines.sql
@@ -1,5 +1,5 @@
-Project Developed by Sanjeev sir's Student
+Project Developed by SM
 any query regarding project 
 
-contact at me phptpoint@gmail.com
-mobile: 9015501897
+contact at 
+Telegram:- https://t.me/samtpoint
